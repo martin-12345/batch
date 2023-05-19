617d147f575308e57d20ab9d05d18bccef4e573b
Martin <martin@mseries.plus.com>
2023-05-19 23:31:23 +0100

    Tidy up

--- src/main/java/com/martin/BatchConfiguration.java
+++ src/main/java/com/martin/BatchConfiguration.java
@@ -1,8 +1,5 @@
 package com.martin;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
@@ -12,28 +9,23 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.partition.support.MultiResourcePartitioner;
-import org.springframework.batch.core.partition.support.Partitioner;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
-import org.springframework.batch.item.support.ClassifierCompositeItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.io.IOException;
 
 @Configuration
 @EnableBatchProcessing
--- src/main/java/com/martin/CustomMultiResourcePartitioner.java
+++ src/main/java/com/martin/CustomMultiResourcePartitioner.java
@@ -31,8 +31,6 @@ import org.springframework.util.Assert;
 
         private Resource[] resources = new Resource[0];
 
-        private String keyName = DEFAULT_IN_KEY_NAME;
-
         /**
          * The resources to assign to each partition. In Spring configuration you
          * can use a pattern to select multiple resources.
@@ -40,15 +38,6 @@ import org.springframework.util.Assert;
          */
         public void setResources(Resource[] resources) {
             this.resources = resources;
-        }
-
-        /**
-         * The name of the key for the file name in each {@link ExecutionContext}.
-         * Defaults to "fileName".
-         * @param keyName the value of the key
-         */
-        public void setKeyName(String keyName) {
-            this.keyName = keyName;
         }
 
         /**
@@ -60,12 +49,12 @@ import org.springframework.util.Assert;
         @Override
         public Map<String, ExecutionContext> partition(int gridSize) {
             Map<String, ExecutionContext> map = new HashMap<>(gridSize);
-            int i = 0, k = 1;
+            int i = 0;
             for (Resource resource : resources) {
                 ExecutionContext context = new ExecutionContext();
                 Assert.state(resource.exists(), "Resource does not exist: " + resource);
 
-                File file = null;
+                File file;
                 try {
                     file = Paths.get(resource.getURI()).toFile();
                 } catch (IOException e) {
